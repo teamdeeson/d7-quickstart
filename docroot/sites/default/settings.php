aac0ab34690e883a0de01e73916ab807f7a26629
Adam Dowling <adamd@deeson.co.uk>
2018-09-16 22:07:27 +0100

    Organise environment detection like D8 Quickstart

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -2,43 +2,15 @@
 
 /**
  * @file
- * The default settings.php loads in real settings.
+ * Default Drupal configuration file sites/default/settings.php.
+ *
+ * You should not normally need to modify this file.
  */
 
-$base_domains = array(
+// Detect the environment.
+require_once DRUPAL_ROOT . '/sites/all/conf/environment.inc';
 
-  // Docker
-  getenv('D4D_HOSTNAME') => 'local',
-
-);
-
-$platform = '';
-$instance = $_SERVER['HTTP_HOST'];
-$env = $base_domains[$_SERVER['HTTP_HOST']];
-
-if (isset($_SERVER['AH_SITE_ENVIRONMENT']) || isset($_ENV['AH_SITE_ENVIRONMENT'])) {
-  $platform = 'acquia';
-  if (file_exists('/var/www/site-php/cope_opsmana/cope_opsmana-settings.inc')) {
-    require_once '/var/www/site-php/cope_opsmana/cope_opsmana-settings.inc';
-  }
-}
-elseif (isset($_SERVER['PANTHEON_ENVIRONMENT']) || isset($_ENV['PANTHEON_ENVIRONMENT'])) {
-  $platform = 'pantheon';
-}
-elseif (getenv('DOCKER_LOCAL')) {
-  $platform = 'docker';
-}
-elseif (file_exists('/var/www/settings/cope/settings.inc')) {
-  $platform = 'vdd';
-  require_once '/var/www/settings/cope/settings.inc';
-}
-
-if (!empty($env) && !empty($platform) && !empty($instance)) {
-  define('SETTINGS_ENVIRONMENT', $env);
-  define('SETTINGS_INSTANCE', $instance);
-  define('SETTINGS_PLATFORM', $platform);
-
-  foreach (glob('sites/all/conf/*.settings.inc') as $file) {
-    require_once $file;
-  }
+// Configure the application.
+foreach (glob(DRUPAL_ROOT . '/sites/all/conf/*.settings.inc') as $file) {
+  require_once $file;
 }
